e80f0cc02701edda5b2dcf22281d273cebb0af9f
TheSonOfCode <lefilsdelhomme777@gmail.com>
2025-04-13 17:51:32 +0100

    update server.js

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+console.log("URI MongoDB reçue :", process.env.MONGODB_URI);
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("Connexion réussie à MongoDB Atlas !");
